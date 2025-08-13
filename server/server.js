5d8b6bebf59ef0299c0493701982e344a70451ee
Akash S <akashspalloor@gmail.com>
2025-08-13 21:09:38 +0530

    update url

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173' }));
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
